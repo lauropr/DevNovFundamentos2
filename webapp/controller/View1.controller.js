d3b1e0550f8c434f82fd3e302c76fa3fb5acc072
lauropribeiro@gmail.com <lauropribeiro@gmail.com>
2023-11-14 21:20:04 -0300

    Binding de agregação

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("demo.fundamentos.controller.View1", {
@@ -18,6 +19,30 @@ sap.ui.define([
                     nacionalidade: "Brasileiro"
                 };
 
+                var oEstados = {
+                    valores: [
+                        { nome: "São Paulo",
+                          abrev: "SP"
+                        },
+                        { nome: "Rio Grande do Sul",
+                          abrev: "RS"
+                        },
+                        { nome: "Minas Gerais",
+                          abrev: "MG"
+                        },
+                        { nome: "Mato Grosso do Sul",
+                          abrev: "MS"
+                        }
+                    ]
+                };
+
+                //declara o modelo Estados e passa os valores do objeto oEstados para a raiz do modelo criado
+                var oModeloEstados = new JSONModel();
+                oModeloEstados.setProperty("/", oEstados);
+                
+                //this = controller 
+                //getView = recupera a instância da nossa view
+                this.getView().setModel(oModeloEstados, "estados");
                 //configura o modelo principal (sem nome) com os dados
                 var oModel = this.getOwnerComponent().getModel();
                 oModel.setProperty("/", oDados);
